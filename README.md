d725196df7bdf054d82d508361f112d9be12e537
Sascha Beining <beining@saltation.de>
2019-06-15 19:17:43 +0200

    Make the slots configurable with folders
    The simplest configuration method I could think of was creating folders
    (via convention) and dropping files into the folders.
    If the file is a plain text file the element will also be plain text,
    if it is an image file the image will be used.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# keldawheels
+# Lucky Wheels
+
+This is a simple slot machine / lucky wheels that can be easily configured by
+dropping files in folders.
+
+## Configuration
+
+* Create a directory called "slots" next to the executable.
+* Create one directory for each wheel (doesn't have to be three)
+  * The name only affects the order of the wheels (left to right)
+* Drop either an image file or a ".txt" file into these folder
+  * The filenames only affect the order of the items on the wheel (top to bottom)
+  * You can mix & match!
--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,47 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad
 import Data.SlotMachine
+import qualified Data.Text as T
+import System.Directory
+import System.FilePath
 
 main :: IO ()
 main = do
-  let slot = Slot $ TextItem "Cherry" : TextItem "Lemon" : TextItem "7" : TextItem "BAR" : []
-  let machine = SlotMachine $ slot : slot : slot : []
+  slots <- buildSlots "slots"
+  let machine = SlotMachine slots
 
   winners <- spin machine
   print $ winners
 
   return ()
+
+buildSlots :: FilePath -> IO [Slot]
+buildSlots path = do
+  contents <- listDirectory path
+
+  setCurrentDirectory path
+  directories <- filterM doesDirectoryExist contents
+  absDirectories <- mapM makeAbsolute directories
+
+  forM absDirectories buildSlot
+
+buildSlot :: FilePath -> IO Slot
+buildSlot path = do
+  contents <- listDirectory $ path
+
+  setCurrentDirectory path
+  files <- filterM doesFileExist contents
+  absFiles <- mapM makeAbsolute files
+
+  slotItems <- forM absFiles buildSlotItem
+  return $ Slot slotItems
+
+buildSlotItem :: FilePath -> IO SlotItem
+buildSlotItem path =
+  case takeExtension path of
+    ".txt" -> do
+      content <- readFile path
+      return $ TextItem <$> T.strip $ T.pack $ content
+    _ -> return $ ImageItem path

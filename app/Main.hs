d40640db0131c322313e57a1d0453b228459e7bc
Sascha Beining <beining@saltation.de>
2019-06-15 23:34:13 +0200

    Implement basic UI with the threepenny-ui library

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad
 import qualified Data.SlotMachine as SlotMachine
+import qualified Data.Text as T
 import System.Directory
 import System.Environment.Executable
 import System.FilePath
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
 
 main :: IO ()
 main = do
@@ -17,5 +21,39 @@ main = do
 
   machine <- SlotMachine.fromFilePath path
 
-  winners <- SlotMachine.spin machine
-  print $ winners
+  startGUI defaultConfig (setup machine)
+
+setup :: SlotMachine.SlotMachine -> Window -> UI ()
+setup machine window = void $ do
+  _ <- return window # set UI.title "Lucky Wheels"
+
+  -- GUI Elements
+  slots <- buildSlotMachineWidget machine
+  button <- UI.button #+ [string "Roll All!"]
+  result <- UI.div
+
+  -- Events
+  on UI.click button $ const $ do
+    slotItems <- liftIO $ SlotMachine.spin machine
+    let winners = buildSlotItemWidget <$> slotItems
+    _ <- element result # set UI.children []
+    element result #+ winners
+
+  -- DOM
+  getBody window #+
+    [ element slots
+    , element result
+    , element button
+    ]
+
+  where
+    buildSlotMachineWidget :: SlotMachine.SlotMachine -> UI Element
+    buildSlotMachineWidget (SlotMachine.SlotMachine slots) = UI.div #+ (buildSlotWidget <$> slots)
+
+    buildSlotWidget :: SlotMachine.Slot -> UI Element
+    buildSlotWidget (SlotMachine.Slot items) = do
+      UI.ul #+ (buildSlotItemWidget <$> items)
+
+    buildSlotItemWidget :: SlotMachine.SlotItem -> UI Element
+    buildSlotItemWidget (SlotMachine.TextItem content) = UI.li #+ [string $ T.unpack content]
+    buildSlotItemWidget (SlotMachine.ImageItem content) = UI.li #+ [string content]
